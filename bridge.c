b8b3bf4f7379ac8f4f1c374fd1494c8183f38ce2
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-04-16 11:57:20 +0900

    aaa

--- bridge.c
+++ bridge.c
@@ -4,49 +4,90 @@
 #include <math.h>
 #include <assert.h>
 
-#include <AL/al.h>
-#include <AL/alc.h>
-
 #include <emscripten.h>
 
-tsf *load_filename(const char *filename)
+EMSCRIPTEN_KEEPALIVE
+tsf *load_sf2(const char *filename)
 {
     return tsf_load_filename(filename);
 }
 
+EMSCRIPTEN_KEEPALIVE
 void init_output(tsf *f)
 {
     tsf_set_output(f, TSF_MONO, 44100, 0);
 }
 
-void note_on(tsf *f, int chan, int key, int velocity)
+EMSCRIPTEN_KEEPALIVE
+void note_on(tsf *f, int chan, int key, float velocity)
 {
     tsf_note_on(f, chan, key, velocity);
 }
 
+EMSCRIPTEN_KEEPALIVE
+void note_off(tsf *f, int chan, int key)
+{
+    tsf_note_off(f, chan, key);
+}
+
+EMSCRIPTEN_KEEPALIVE
 void render_float(tsf *f, float *buf, int samples)
 {
     tsf_render_float(f, buf, samples, 0);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void render_short(tsf *f, short *buf, int samples)
+{
+    tsf_render_short(f, buf, samples, 0);
 }
 
 #define BUF_COUNT 2
 ALuint sources[1];
 ALuint buffers[BUF_COUNT];
 
+tsf *SoundFont = NULL;
+
+#define key_min 69
+#define key_max (69 + 12)
+
+void *random_note_on(void *ptr)
+{
+    printf("hi\n");
+    static int key_current = key_min;
+    static int key_prev = -1;
+
+    while (1)
+    {
+        int key = key_current;
+        if (++key_current == key_max)
+        {
+            key_current = key_min;
+        }
+        key_prev = key;
+        float vel = 1.0;
+        int chan = 1;
+
+        if (key_prev != 1)
+        {
+            note_off(SoundFont, chan, key_prev);
+        }
+
+        note_on(SoundFont, chan, key, vel);
+
+        emscripten_sleep(300);
+    }
+}
+
 void fill_buffer(ALuint buffer)
 {
-    const int audioFreq = 441;
     const int sampleRate = 44100;
     const int bufLen = sampleRate / 2;
-    signed char buf[bufLen];
+    short buf[bufLen];
 
-    for (int i = 0; i < bufLen; i++)
-    {
-        float t = i / (float)sampleRate;
-        buf[i] = (signed char)(sinf(2 * M_PI * audioFreq * t) * 30.0);
-    }
+    render_short(SoundFont, buf, bufLen);
 
-    alBufferData(buffer, AL_FORMAT_MONO8, buf, sizeof(buf), sampleRate);
+    alBufferData(buffer, AL_FORMAT_MONO16, buf, sizeof(buf), sampleRate);
 }
 
 void audio_loop(void)
@@ -68,52 +109,61 @@ void audio_loop(void)
     }
 }
 
-int main()
-{
-    int major, minor;
-    alcGetIntegerv(NULL, ALC_MAJOR_VERSION, 1, &major);
-    alcGetIntegerv(NULL, ALC_MINOR_VERSION, 1, &minor);
+// int main()
+// {
+//     SoundFont = load_sf2("FluidR3_GM.sf2");
+//     init_output(SoundFont);
 
-    assert(major == 1);
+//     int major, minor;
+//     alcGetIntegerv(NULL, ALC_MAJOR_VERSION, 1, &major);
+//     alcGetIntegerv(NULL, ALC_MINOR_VERSION, 1, &minor);
 
-    printf("ALC version: %i.%i\n", major, minor);
-    printf("Default device: %s\n", alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER));
+//     assert(major == 1);
 
-    ALCdevice *device = alcOpenDevice(NULL);
-    ALCcontext *context = alcCreateContext(device, NULL);
-    alcMakeContextCurrent(context);
+//     printf("ALC version: %i.%i\n", major, minor);
+//     printf("Default device: %s\n", alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER));
 
-    printf("OpenAL version: %s\n", alGetString(AL_VERSION));
-    printf("OpenAL vendor: %s\n", alGetString(AL_VENDOR));
-    printf("OpenAL renderer: %s\n", alGetString(AL_RENDERER));
+//     ALCdevice *device = alcOpenDevice(NULL);
+//     ALCcontext *context = alcCreateContext(device, NULL);
+//     alcMakeContextCurrent(context);
 
-    ALfloat listenerPos[] = {0.0, 0.0, 1.0};
-    ALfloat listenerVel[] = {0.0, 0.0, 0.0};
-    ALfloat listenerOri[] = {0.0, 0.0, -1.0, 0.0, 1.0, 0.0};
-    ALfloat listenerGain = 0.1;
+//     printf("OpenAL version: %s\n", alGetString(AL_VERSION));
+//     printf("OpenAL vendor: %s\n", alGetString(AL_VENDOR));
+//     printf("OpenAL renderer: %s\n", alGetString(AL_RENDERER));
 
-    alListenerfv(AL_POSITION, listenerPos);
-    alListenerfv(AL_VELOCITY, listenerVel);
-    alListenerfv(AL_ORIENTATION, listenerOri);
-    alListenerf(AL_GAIN, listenerGain);
+//     ALfloat listenerPos[] = {0.0, 0.0, 1.0};
+//     ALfloat listenerVel[] = {0.0, 0.0, 0.0};
+//     ALfloat listenerOri[] = {0.0, 0.0, -1.0, 0.0, 1.0, 0.0};
+//     ALfloat listenerGain = 0.1;
 
-    // prepare buffers
-    alGenBuffers(BUF_COUNT, buffers);
+//     alListenerfv(AL_POSITION, listenerPos);
+//     alListenerfv(AL_VELOCITY, listenerVel);
+//     alListenerfv(AL_ORIENTATION, listenerOri);
+//     alListenerf(AL_GAIN, listenerGain);
 
-    // prepare sources
-    alGenSources(1, sources);
-    assert(alIsSource(sources[0]));
+//     // prepare buffers
+//     alGenBuffers(BUF_COUNT, buffers);
 
-    // initial data
-    ALuint source = sources[0];
-    for (int i = 0; i < BUF_COUNT; i++)
-    {
-        ALuint buffer = buffers[i];
-        fill_buffer(buffer);
-    }
-    alSourceQueueBuffers(source, 2, buffers);
+//     // prepare sources
+//     alGenSources(1, sources);
+//     assert(alIsSource(sources[0]));
 
-    alSourcePlay(sources[0]);
+//     // start note thread
+//     pthread_t thread_note;
+//     pthread_create(&thread_note, NULL, random_note_on, NULL);
 
-    emscripten_set_main_loop(audio_loop, 15, 0);
-}
+//     // initial data
+//     ALuint source = sources[0];
+//     for (int i = 0; i < BUF_COUNT; i++)
+//     {
+//         ALuint buffer = buffers[i];
+//         fill_buffer(buffer);
+//     }
+//     alSourceQueueBuffers(source, 2, buffers);
+
+//     alSourcePlay(sources[0]);
+
+//     emscripten_set_main_loop(audio_loop, 15, 0);
+
+//     pthread_join(thread_note, NULL);
+// }
